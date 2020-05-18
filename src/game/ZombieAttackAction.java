2c2bef99698a21518bd7bd91c1ffc015fa3aecc3
Teh <chaelqi89@hotmail.com>
2020-05-18 10:43:30 +0800

    Pushed 1st version of ZombieAttackAction

--- src/game/ZombieAttackAction.java
+++ src/game/ZombieAttackAction.java
@@ -1,26 +1,50 @@
-/**
- * 
- */
 package game;
 
+import java.util.Random;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actions;
 import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Weapon;
 
 /**
- * @author Teh Qi Yuan
- * 
- * A attack action class for the zombie
- * 
- * TODO: need to check for losing arms probability
- * 	- loss weapon when both arm loss
- *  - 50% of dropping weapon if one arm loss
- *  - probability of punching is halved if one arm is loss
- *
+ * Special Action for attacking other Actors.
  */
 public class ZombieAttackAction extends AttackAction {
 
 	public ZombieAttackAction(Actor target) {
 		super(target);
-		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public String execute(Actor actor, GameMap map) {
+
+		Weapon weapon = actor.getWeapon(); // automatically return intrinsic weapon if actor no weapon
+
+		if (rand.nextBoolean()) {  // randomly let the actor miss his target
+			return actor + " misses " + target + ".";
+		}
+
+		int damage = weapon.damage();      
+		String result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
+
+		target.hurt(damage);
+		if (!target.isConscious()) {
+			Item corpse = new PortableItem("dead " + target, '%');
+			map.locationOf(target).addItem(corpse);
+			
+			Actions dropActions = new Actions();
+			for (Item item : target.getInventory())
+				dropActions.add(item.getDropAction());
+			for (Action drop : dropActions)		
+				drop.execute(target, map);
+			map.removeActor(target);	
+			
+			result += System.lineSeparator() + target + " is killed.";
+		}
+
+		return result;
+	}
 }
